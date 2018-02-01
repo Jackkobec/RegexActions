93a852509cea2c9c7ae801eb3af303c7d292efbe
jack <e.kobets@finik.pro>
2018-02-01 14:26:23 +0200

    PasswordRegex.

--- src/main/java/PasswordRegex.java
+++ src/main/java/PasswordRegex.java
@@ -1,4 +1,5 @@
-
+import java.util.Arrays;
+import java.util.List;
 
 public class PasswordRegex {
     public static void main(String[] args) {
@@ -32,5 +33,28 @@ public class PasswordRegex {
          */
         final String passwordRegex2 = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
         System.out.println(password.matches(passwordRegex2));
+
+        /**
+         * Bank requirements
+         * ^               # start-of-REGEX
+         (?=.*[0-9])       # a digit must occur at least once
+         (?=.*[a-z])       # a lower case letter must occur at least once
+         (?=.*[A-Z])       # an upper case letter must occur at least once
+         (?=.*[@#$%^&+=])  # a special character must occur at least once
+         (?=\S+$)          # no whitespace allowed in the entire string
+         .{8,}             # anything, at least eight places though
+         $                 # end-of-REGEX
+         */
+        final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\\\\!\"#$%&\'()*+,-./:;<=>?@\\[\\]|^_`{}~])(?=\\S+$).{8,}$";
+
+        final List<String> correctPasswords = Arrays.asList("", "       ", "4444", "77777777", "qqqqqqqq", "QQQQQQQQ", "Qw777777",
+                "_-++++++++", "!qqqqqqq", "!QQQQQQQ", "Qw_ 77777");
+
+        correctPasswords.stream()
+                .forEach(correctPassword -> {
+                            System.out.println(correctPassword + ": " + !correctPassword.matches(PASSWORD_REGEX));
+//                            Assert.assertEquals(true, !correctPassword.matches(PASSWORD_REGEX));
+                        }
+                );
     }
 }
